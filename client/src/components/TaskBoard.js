0a46f7ba5eeb19d0f0f51476f3335b9ec6cacc7e
Romaan <romaan567@hotmail.com>
2020-11-22 14:58:17 -0500

    implementing subtasks

--- client/src/components/TaskBoard.js
+++ client/src/components/TaskBoard.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import { Menu, Dropdown, Button, message, Table } from 'antd';
+import { Table } from 'antd';
 
 export default function TaskBoard() {
   let auth = useSelector((state) => state.auth);
   let [tasks, setTasks] = useState([]);
   let [tableData, setTableData] = useState([]);
+  let [subtasks, setSubtasks] = useState([]);
+  let [teamId, setTeamId] = useState('');
 
   const clickupApi = axios.create({
     baseURL: 'https://cors-duck.herokuapp.com/https://api.clickup.com/api/v2',
@@ -44,7 +46,7 @@ export default function TaskBoard() {
 
           setTableData((oldArray) => [
             ...oldArray,
-            { key: res.data.id, taskName: res.data.name },
+            { key: res.data.id, taskName: res.data.name, subtasks: [] },
           ]);
         });
       });
@@ -53,7 +55,51 @@ export default function TaskBoard() {
     fetchTasksById();
   }, []);
 
-  console.log(tableData);
+  // Sets Team ID
+  useEffect(() => {
+    const saveTeamId = async () => {
+      await clickupApi
+        .get(`/team`)
+        .then((res) => setTeamId(res.data.teams[0].id));
+    };
+    saveTeamId();
+  }, []);
+
+  useEffect(() => {
+    // Clickup API is bugged for subtasks so we're gonna have to do this the long way
+    //* 1. Get team ID
+    //* 2. Get all subtasks from the team
+    //! 3. Check individual subtasks for matching parent ID to existing tracked task ID
+    //! 4. If theres a match, push to subtasks array inside object
+
+    const fetchSubtasks = () => {
+      let trackedTasks = tasks;
+      // console.log('tracked tasks', trackedTasks);
+
+      for (let i = 0; i < 5; i++) {
+        clickupApi
+          .get(`/team/${teamId}/task?page=${i}&subtasks=true`)
+          .then((res) => {
+            if (res.data.tasks.length != 0) {
+              // console.log(`subtasks for team page ${i}`, res.data.tasks);
+
+              // Mapping through all subtasks
+              res.data.tasks.map((subtask) => {
+                // Mapping through all tracked tasks
+                tasks.map((trackedTask) => {
+                  console.log('subtask', subtask);
+                  console.log('Task from state', trackedTask);
+                });
+              });
+            }
+          });
+      }
+    };
+
+    if (tasks != [] && teamId != '') {
+      fetchSubtasks();
+    }
+  }, [tasks, teamId]);
 
   return <Table columns={columns} dataSource={tableData} />;
 }
